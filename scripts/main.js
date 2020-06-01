0162d49db0602fd1ec92b7bdfd9d67184a6098e6
isaac grimaldo <isaacgrimaldo432@gmail.com>
2020-06-01 18:10:55 -0500

    ajustes de tamaño para celulares

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+/*programacion del navegador*/
+/*function saludo(){
+    alert("hola");
+};
+function nav(){
+    let elementos =  document.querySelectorAll("#temas  li , span");
+    for(let i =0 ; i < elementos.length; i++){
+       elementos[i].onclick = saludo;
+    }
+};
+window.onload = nav;(como usar los selectores de texto)*/
+
+
+
+function nav(){
+         let  pags = document.querySelectorAll("#temas li");
+
+            pags[0].addEventListener("click",function(){
+                window.open("peleas.html","blanck");
+            });
+
+            pags[1].addEventListener("click",function(){
+                window.open("personajes.html","blanck");
+            });
+
+            pags[2].addEventListener("click",function(){
+                window.open("pag3.html","blanck");
+            });
+}; 
+
+
+window.onload = nav;
+
+/*progracion de la navegacion*/
